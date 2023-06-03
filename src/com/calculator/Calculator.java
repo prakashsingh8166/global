53c54f991de520f08543cacc87351a5a2ceda628
unknown <singh.prakash816@gmail.com>
2023-06-03 11:36:15 +0530

    my first  pushcode

---
+++ src/com/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.calculator;
+
+public class Calculator extends Dframe {
+	public static void main(String[] args) {
+		int a=1 , b=2;
+		
+		System.out.println("addition of two number :" +(a+b));
+		System.out.println("substraction of number :" + (a-b));
+		System.out.println("divition of numbers :" + (a/a));
+		
+	}
+
+}
